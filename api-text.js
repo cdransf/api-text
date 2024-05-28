62444da9301ffb5aa0ed8ea47d0743382d73be2c
Cory Dransfeldt <hi@coryd.dev>
2024-05-28 09:00:17 -0700

    chore: simplify logic

--- api-text.js
+++ api-text.js
@@ -1,11 +1,8 @@
 class ApiText extends HTMLElement {
   static tagName = 'api-text'
 
-  static register(tagName, registry) {
-    if(!registry && ('customElements' in globalThis)) {
-      registry = globalThis.customElements;
-    }
-    registry?.define(tagName || this.tagName, this);
+  static register(tagName = this.tagName, registry = globalThis.customElements) {
+    registry.define(tagName, this)
   }
 
   static attr = {
@@ -13,50 +10,39 @@ class ApiText extends HTMLElement {
   }
 
   get url() {
-    return this.getAttribute(ApiText.attr.url) || '';
+    return this.getAttribute(ApiText.attr.url) || ''
   }
 
   async connectedCallback() {
-    if (this.shadowRoot) return;
-    let shadowroot = this.attachShadow({ mode: 'open' })
-    let slot = document.createElement('slot')
-    shadowroot.appendChild(slot)
+    if (this.shadowRoot) return
 
-    const data = { ...(await this.data) };
+    this.attachShadow({ mode: 'open' }).appendChild(document.createElement('slot'))
+
     const loading = this.querySelector('.loading')
     const content = this.querySelector('.content')
-    const value = data['content']
     const cacheKey = this.url || 'api-text-cache'
     const cache = sessionStorage?.getItem(cacheKey)
     const loadText = (string) => {
-      if (cache) {
-        loading.style.display = 'none'
-        content.style.display = 'block'
-      }
-
-      if (typeof string === 'undefined') {
-        loading.style.display = 'none'
-        content.style.display = 'none'
-      }
-
-      if (string && content.style.display !== 'block') {
-        loading.style.display = 'none'
-        content.style.display = 'block'
-        content.innerHTML = value
-      } else if (string && content.style.display === 'block') {
-        content.innerHTML = value
-      }
+      loading.style.display = string ? 'none' : ''
+      content.style.display = string ? 'block' : 'none'
+      if (string) content.innerHTML = string
     }
 
-    loadText(cache);
-    loadText(value);
+    if (cache) loadText(JSON.parse(cache))
 
-    sessionStorage?.setItem(cacheKey, JSON.stringify(value))
+    try {
+      const data = await this.data
+      const value = data.content
+      loadText(value)
+      sessionStorage?.setItem(cacheKey, JSON.stringify(value))
+    } catch (error) {
+      loadText()
+    }
   }
 
   get data() {
-    return fetch(this.url, { type: 'json' }).then((data) => data.json()).catch((e) => {})
+    return fetch(this.url).then(response => response.json()).catch(() => ({}))
   }
 }
 
-ApiText.register();
+ApiText.register()
